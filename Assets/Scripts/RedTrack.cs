61a775ea15b345f5e0aeadd2e893c71320e138e9
akah <akacircus@163.com>
2021-11-24 03:21:20 +0800

    rotate

--- Assets/Scripts/RedTrack.cs
+++ Assets/Scripts/RedTrack.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RedTrack : MonoBehaviour
 {
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+
+    public bool rotating;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +19,7 @@ public class RedTrack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateAroundRight();
     }
 
     public Vector3 GetRedTrackPosition()
@@ -24,6 +30,41 @@ public class RedTrack : MonoBehaviour
     public Vector3 GetRedTrackRotation()
     {
         return this.gameObject.transform.eulerAngles;
+    }
+
+    public void RotateAroundLeft()
+    {
+        if(rotating)
+        {
+            transform.RotateAround(leftPoint.transform.position,Vector3.back, speed*Time.deltaTime);
+        }
+    }
+
+    public void RotateAroundRight()
+    {
+        if (rotating)
+        {
+            transform.RotateAround(rightPoint.transform.position, Vector3.forward, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision:" + collision.transform.name);
+        if (collision.transform.tag == "ground")
+        {
+            rotating = false;
+        }
+    }
+
+    public void SetRotating(bool rotate)
+    {
+        rotating = rotate;
+    }
+
+    private void OnMouseDown()
+    {
+        rotating = true;
     }
 
 }
